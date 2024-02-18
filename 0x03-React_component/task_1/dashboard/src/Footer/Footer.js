eb51b97f0a8cfc97ae7989af8887b299f275de43
fabianphilp <fabianphilip92@yahoo.com>
2024-02-19 00:12:34 +0100

    Reusable comments & specialization

--- 0x03-React_component/task_1/dashboard/src/Footer/Footer.js
+++ 0x03-React_component/task_1/dashboard/src/Footer/Footer.js
@@ -1,15 +1,13 @@
-import React from "react";
-import "./Footer.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import React from 'react';
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function Footer() {
   return (
-    <div className="App-footer">
-      <p>
-        Copyright {getFullYear()} - {getFooterCopy(true)}
-      </p>
-    </div>
-  );
+    <footer className='App-footer'>
+      <p>{`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}</p>
+    </footer>
+  )
 }
 
 export default Footer;
