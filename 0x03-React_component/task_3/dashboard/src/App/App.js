7ac8e7a8cf3f5ae5d684d63e72256e5d7d9dd1c8
fabianphilp <fabianphilip92@yahoo.com>
2024-02-19 00:10:34 +0100

    Reusable comments & specialization

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -7,7 +7,8 @@ import Login from "../Login/Login";
 import "./App.css";
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
-
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from "../BodySection/BodySection";
 
 const listCourses = [
   {id: 1, name: 'ES6', credit: 60},
@@ -50,7 +51,19 @@ class App extends React.Component {
       <div className="App">
         <Header />
       </div>
-      <div className="App-body">{!this.props.isLoggedIn ? <Login /> : <CourseList listCourses={listCourses}/>}</div>
+      <div className="App-body">
+        {!this.props.isLoggedIn ? 
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login /> 
+          </BodySectionWithMarginBottom> : 
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList listCourses={listCourses}/>
+          </BodySectionWithMarginBottom>
+        }
+        <BodySection title="News from the School">
+          <p>New News</p>
+        </BodySection>
+      </div>
       <div className="App-footer">
         <Footer />
       </div>
